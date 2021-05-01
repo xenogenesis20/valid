a05ad6c1baaeb56846f45636b26123193cd81028
xenogenesis20 <xenogenesis20@outlook.com>
2021-05-01 20:30:08 +0000

    almost done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let submitBtn = document.querySelector("#submitBtn");
+
+let form = document.querySelector("form");
+
+let userInput = document.querySelectorAll("input");
+
+let errorMsg = document.querySelector("#errorMsg");
+
+function submitForm() {
+  form.submit();
+}
+
+submitBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+  areAllInputsFilled();
+  if (areAllInputsFilled()) {
+    submitForm();
+  }
+});
+
+function areAllInputsFilled() {
+  let allFilled = true;
+
+  userInput.forEach(input => {
+    if (
+      input.value == "" ||
+      null ||
+      undefined ||
+      input.validity.tooShort ||
+      input.validity.tooLong
+    ) {
+      input.reportValidity();
+      errorMsg.classList = "alert alert-danger d-block";
+      input.style.backgroundColor = "#f8d7da";
+      allFilled = false;
+      return false;
+    }
+  });
+  return allFilled;
+}
---
+++ src/attempts.js
@@ -0,0 +1,21 @@
+  //   e.preventDefault();
+  //   userInput.forEach(input => {
+  //     if (input.classList.contains("valid")) {
+  //       submitForm();
+  //     } else {
+  //       errorMsg.classList = "alert alert-danger d-block";
+  //     }
+  //   });
+ 
+ 
+ //   let userInputArr = Array.from(userInput).map(input => {
+  //     if (input) {
+  //       if (input.checkValidity()) {
+  //         submitForm();
+  //       } else {
+  //         errorMsg.classList = "alert alert-danger d-block";
+  //       }
+  //     }
+  //   });
+
+

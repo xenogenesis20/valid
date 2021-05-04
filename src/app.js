25beddce22cdd4150796cc954d8dd26924918336
xenogenesis20 <xenogenesis20@outlook.com>
2021-05-04 00:43:03 +0000

    done

--- src/app.js
+++ src/app.js
@@ -1,47 +1,75 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let submitBtn = document.querySelector("#submitBtn");
-
-let form = document.querySelector("form");
-
-let userInput = document.querySelectorAll("input");
-
-let errorMsg = document.querySelector("#errorMsg");
-
-function submitForm() {
-  form.submit();
+function displayError(msg) {
+  let errorMsg = document.querySelector("#errorMsg");
+  errorMsg.classList.contains("d-none") && errorMsg.classList.toggle("d-block");
+  errorMsg.innerHTML += `<p>${msg}</p>`;
 }
 
-submitBtn.addEventListener("click", function(e) {
+const submitForm = e => {
   e.preventDefault();
-  areAllInputsFilled();
-  if (areAllInputsFilled()) {
-    submitForm();
+
+  let errorMsg = document.querySelector("#errorMsg");
+  !errorMsg.classList.contains("d-none") && errorMsg.classList.toggle("d-none");
+  errorMsg.innerHTML += "";
+
+  document
+    .querySelectorAll("input")
+    .forEach(elem => elem.classList.remove("is-invalid"));
+
+  let inputcard = document.querySelector("#inputcard");
+  let ccv = document.querySelector("#CCV");
+  let firstName = document.querySelector("#firstName");
+  let lastName = document.querySelector("#lastName");
+  let address = document.querySelector("#inputAddress2");
+  let city = document.querySelector("#inputCity");
+  let state = document.querySelector("#inputState");
+  let zip = document.querySelector("#inputZip");
+  let message = document.querySelector("#message");
+  let cardDiv = document.querySelector("#cardDiv");
+  let theMsg = document.createTextNode("Error");
+
+  if (inputcard.value.length != 16) {
+    displayError("Card has to be 16 digits.");
+    inputcard.classList.add("is-invalid");
+    cardDiv.appendChild(theMsg);
   }
-});
+  if (ccv.value.length != 3) {
+    displayError("CCV must be 3 digits.");
+    ccv.classList.add("is-invalid");
+  }
+  if (firstName.value.length < 2) {
+    displayError("name must be at least 2 characters.");
+    firstName.classList.add("is-invalid");
+  }
+  if (lastName.value.length < 2) {
+    displayError("last name must be at least 2 characters.");
+    lastName.classList.add("is-invalid");
+  }
+  if (address.value.length < 2) {
+    displayError("address must be at least 2 characters.");
+    address.classList.add("is-invalid");
+  }
+  if (city.value.length < 2) {
+    displayError("city must be at least 2 characters.");
+    city.classList.add("is-invalid");
+  }
+  if (state.value == "Choose...") {
+    displayError("state must be selected.");
+    state.classList.add("is-invalid");
+  }
+  if (zip.value.length < 2) {
+    displayError("zip must be at least 2 characters.");
+    zip.classList.add("is-invalid");
+  }
+  if (message.value.length < 1) {
+    displayError("message cannot be blank.");
+    message.classList.add("is-invalid");
+  }
+};
 
-function areAllInputsFilled() {
-  let allFilled = true;
-
-  userInput.forEach(input => {
-    if (
-      input.value == "" ||
-      null ||
-      undefined ||
-      input.validity.tooShort ||
-      input.validity.tooLong
-    ) {
-      input.reportValidity();
-      errorMsg.classList = "alert alert-danger d-block";
-      input.style.backgroundColor = "#f8d7da";
-      allFilled = false;
-      return false;
-    }
-  });
-  return allFilled;
-}
+document.querySelector("#submitBtn").addEventListener("click", submitForm);
---
+++ src/app2.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+let submitBtn = document.querySelector("#submitBtn");
+
+let form = document.querySelector("form");
+
+let userInput = document.querySelectorAll("input");
+
+let errorMsg = document.querySelector("#errorMsg");
+
+let card = document.querySelector("#card");
+
+function submitForm() {
+  form.submit();
+}
+
+submitBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+  areAllInputsFilled();
+  if (areAllInputsFilled()) {
+    submitForm();
+  }
+});
+
+function areAllInputsFilled() {
+  let allFilled = true;
+
+  userInput.forEach(input => {
+    if (
+      input.value == "" ||
+      null ||
+      undefined ||
+      input.validity.tooShort ||
+      input.validity.tooLong
+    ) {
+      input.reportValidity();
+      errorMsg.classList = "alert alert-danger d-block";
+      input.style.backgroundColor = "#f8d7da";
+      allFilled = false;
+      return false;
+    }
+  });
+  return allFilled;
+}
